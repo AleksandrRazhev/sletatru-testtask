26ad5e7021c3599002f965120931feacd03bbcd7
AleksandrRazhev <alexandrrazh@gmail.com>
2022-11-05 13:59:42 +0300

    variable border radius added

--- test-task/src/components/Booking/Booking.tsx
+++ test-task/src/components/Booking/Booking.tsx
@@ -3,7 +3,9 @@ import style from "./Booking.module.scss";
 export const Booking: React.FC = (): JSX.Element => {
   return (
     <section className={style.booking}>
-      <h2 className={style.title}>Бронирование тура</h2>
+      <div className={style.container}>
+        <h2 className={style.title}>Бронирование тура</h2>
+      </div>
     </section>
   );
 };
