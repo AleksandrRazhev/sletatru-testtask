1cc4cb0eaa2b9018abb2e7dec1efd243517e3599
AleksandrRazhev <alexandrrazh@gmail.com>
2022-11-07 19:24:05 +0300

    768 screen has started

--- test-task/src/components/Booking/Booking.tsx
+++ test-task/src/components/Booking/Booking.tsx
@@ -13,12 +13,14 @@ export const Booking: React.FC = (): JSX.Element => {
       <div className={style.container}>
         <div className={style.tourInfo}>
           <h2 className={style.bookingTitle}>Бронирование тура</h2>
-          <StickyTourInfo />
-          <div className={style.help}>
-            <h3 className={style.helpTitle}>Нужна помощь?</h3>
-            <a href="tel:+78003339963" className={style.helpPhone}>
-              8 800 333 99 63
-            </a>
+          <div>
+            <StickyTourInfo />
+            <div className={style.help}>
+              <h3 className={style.helpTitle}>Нужна помощь?</h3>
+              <a href="tel:+78003339963" className={style.helpPhone}>
+                8 800 333 99 63
+              </a>
+            </div>
           </div>
         </div>
         <form
